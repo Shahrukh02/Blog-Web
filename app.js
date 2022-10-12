2d24545033386d97cd81569cca7a0b1ddf05a702
Muhammad Shahrukh <104302450+Shahrukh02@users.noreply.github.com>
2022-10-12 10:30:33 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+  
+  
+ 
+//   import { initializeApp } from "https://www.gstatic.com/firebasejs/9.12.0/firebase-app.js";
+//   import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.12.0/firebase-analytics.js";
+  
+
+//   const firebaseConfig = {
+//     
+//   };
+
+//   // Initialize Firebase
+//   const app = initializeApp(firebaseConfig);
+//   const analytics = getAnalytics(app);
